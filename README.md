e6c34e5901c9c8e4bd23cb2ab141f781474ec434
baldierot <baldierot@gmail.com>
2026-01-16 00:13:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Link Browser
+
+Link Browser is a minimalist Android application designed to open web links without appearing in the app launcher.
+
+## Building
+
+This project can be built using Android Studio or the included Gradle wrapper.
+
+To build the project from the command line, run the following command in the project's root directory:
+
+```bash
+./gradlew assembleRelease
+```
+
+### Dependencies
+
+*   Android SDK: `34`
+*   Android Gradle Plugin: `8.2.0`
+*   Java: `8`
---
+++ app/src/main/java/com/recovery/linkbrowser/MainActivity.java
@@ -0,0 +1,45 @@
+package com.recovery.linkbrowser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+
+        if (data != null) {
+            webView.loadUrl(data.toString());
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = findViewById(R.id.webview);
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
