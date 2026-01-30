3b55715fb527cf8634e4a937d6c0b6bb41ed2f1b
baldierot <baldierot@gmail.com>
2026-01-30 21:46:29 +0200

    add "Copy Link Url" to an ancient-looking context menu

--- app/src/main/java/com/recovery/linkbrowser/MainActivity.java
+++ app/src/main/java/com/recovery/linkbrowser/MainActivity.java
@@ -1,9 +1,15 @@
 package com.recovery.linkbrowser;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,14 +22,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webview);
+        registerForContextMenu(webView);
+
         WebSettings webSettings = webView.getSettings();
-        
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setSupportZoom(true);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setDisplayZoomControls(false);
-        
+
         webView.setWebViewClient(new WebViewClient());
 
         Intent intent = getIntent();
@@ -33,6 +40,27 @@ public class MainActivity extends Activity {
             webView.loadUrl(data.toString());
         } else {
             finish();
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        WebView.HitTestResult result = ((WebView) v).getHitTestResult();
+
+        if (result.getType() == WebView.HitTestResult.SRC_ANCHOR_TYPE ||
+            result.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+
+            String url = result.getExtra();
+            menu.setHeaderTitle(url);
+
+            menu.add(0, 1, 0, "Copy Link URL").setOnMenuItemClickListener(item -> {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("URL", url);
+                clipboard.setPrimaryClip(clip);
+                return true;
+            });
         }
     }
 
